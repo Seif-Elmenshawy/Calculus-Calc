fb9c29df889fd55a1c6524451fe0a08527db04fb
Seif <seif.ahmed.3849@gmail.com>
2025-08-10 16:07:49 +0300

    finished the use of api and async function for derivative

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-async function fetchData(){
+async function fetchintegrationData(){
     try{
         const integralQuestion = document.getElementById("integration").value.toLowerCase().trim();
-        const response= await fetch(`https://newton.vercel.app/api/v2/integrate/${integralQuestion}`);
+        const encoded = encodeURIComponent(integralQuestion)
+        const response= await fetch(`https://newton.vercel.app/api/v2/integrate/${encoded}`);
         
         if(!response.ok){
             throw new Error("Couldn't fetch the data");
         }
         const data= await response.json();
         const result = document.getElementById("intgrateresult");
+        const answer = data.result;
+        result.innerText=answer;
+
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+async function fetchDerivativeData(){
+    try{
+        const integralQuestion = document.getElementById("derivativequestion").value.toLowerCase().trim();
+        const encoded = encodeURIComponent(integralQuestion)
+        const response= await fetch(`https://newton.vercel.app/api/v2/integrate/${encoded}`);
+        
+        if(!response.ok){
+            throw new Error("Couldn't fetch the data");
+        }
+        const data= await response.json();
+        const result = document.getElementById("derivativeans");
         const answer = data.result;
         result.innerText=answer;
 
