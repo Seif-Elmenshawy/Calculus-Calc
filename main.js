89a11434ef07a3145033cf5335dc8a0aa6425940
Seif <seif.ahmed.3849@gmail.com>
2025-08-10 18:24:34 +0300

    fixed the error in inseerting the results, and added some event listeners

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ async function fetchDerivativeData(){
             throw new Error("Couldn't fetch the data");
         }
         const data= await response.json();
-        const result = document.getElementById("intgrateresult");
+        const result = document.getElementById("derivativeans");
         const answer = data.result;
         result.innerText=answer;
 
@@ -43,3 +43,38 @@ function changetheme(){
     document.getElementById("footer").classList.toggle("darktheme")
     document.getElementsById("hero").classList.toggle("darktheme")
 }
+function re(){
+    let email = document.getElementById("email").value
+    let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    let validation = pattern.test(email)
+    if (validation === false){
+        alert("insert a valid email")
+    }
+    else{
+        return true
+    }
+}
+const element = document.getElementById("integration");
+element.addEventListener("paste", function(){
+    alert("you pasted something");
+})
+
+document.getElementById("contact").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent the form from submitting/reloading
+  document.getElementById("main").style.display = "none";
+
+  const newHeading = document.createElement("h1");
+  newHeading.innerText = "Thanks for sharing your opinion";
+  document.body.appendChild(newHeading);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("contact").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent the form from submitting/reloading
+  document.getElementById("main").style.display = "none";
+
+  const newHeading = document.createElement("h1");
+  newHeading.innerText = "Thanks for sharing your opinion";
+  document.body.appendChild(newHeading);
+});
+});
